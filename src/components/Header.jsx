be0c7711a24bf9a4c1072b7d60c59c7a3cc8ebda
pangphaichit <pg_grj@hotmail.com>
2024-10-26 19:30:56 +0700

    feat: add dropdown menu with hamburger icon for mobile navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,43 @@
+import { Menu } from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdownmenu";
+
 export function Header() {
     return (
       
-      <header className="flex items-center bg-white border-b  p-4 justify-between ">
-        <div className="logo pl-20 ">
+      <header className="flex items-center bg-white border-b justify-between h-16 px-6 ">
+        <div className="logo lg:pl-20">
           <img src="/hh..svg" alt="Logo"/>
         </div>
         <nav>
-         <ul className="nav-list flex justify-end space-x-4 pr-20">
-            <li className="hidden md:flex space-x-4">
+         <ul className="nav-list justify-end px-6 py-3 hidden sm:flex space-x-4">
+            <li className="hidden sm:flex space-x-4">
             <a href="/login" className="px-9 py-3 rounded-full border border-slate-300 bg-white text-black hover:bg-gray-400 hover:text-white transition-colors">Log in</a>
             </li>
             <li className="hidden md:flex space-x-4">
             <a href="/login" className="px-8 py-3  rounded-full border border-slate-300 bg-white text-black hover:bg-gray-400 hover:text-white transition-colors"> Sign up</a>
             </li>
           </ul>
-          <button className="md:hidden">Menu</button>
+          <DropdownMenu className="flex">
+          <DropdownMenuTrigger className="sm:hidden focus:outline-none">
+          <Menu />
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="sm:hidden w-screen rounded-none mt-4 flex flex-col gap-6 py-10 px-6">
+          <a href="/login" className="px-8 py-4 rounded-full text-center text-foreground border border-foreground hover:border-muted-foreground hover:text-muted-foreground transition-colors">
+            Log in
+          </a>
+          <a
+            href="/signup" className="px-8 py-4 bg-foreground text-center text-white rounded-full hover:bg-muted-foreground transition-colors">
+            Sign up
+          </a>
+        </DropdownMenuContent>
+      </DropdownMenu>
         </nav>
       </header>
     );
