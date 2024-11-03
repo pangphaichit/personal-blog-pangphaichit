c5b75e4cbae41570d1bfdcbfc8c145b63c0b455a
pangphaichit <pg_grj@hotmail.com>
2024-11-03 21:13:47 +0700

    feat: set up routing for the application

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,25 @@
-
-import { Header } from "./components/Header";
-import { HeroSection } from "./components/HeroSection";
-import {Article} from "./components/Article";
-import {Footer} from "./components/Footer";
-
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HomePage } from "./pages/HomePage";
+import { ViewPostPage } from "./pages/ViewPostPage";
+import { NotFoundPage } from "./pages/NotFoundPage";
+import { Toaster } from "sonner";
 
 function App() {
 return (
-    <>
-    <div>
-      <Header />
-      <div className="flex-grow">
-        <HeroSection />
-        <Article />
-      </div>
-      <Footer />
-     </div>
-    </>
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/post/:postId" element={<ViewPostPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Router>
+      <Toaster
+        toastOptions={{
+          unstyled: true,
+        }}
+      />
+    </div>
   );
 }
 
