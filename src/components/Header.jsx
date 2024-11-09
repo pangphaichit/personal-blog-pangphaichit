ea73f6a4e44f35b97c7e3ed580692aba4f06b293
pangphaichit <pg_grj@hotmail.com>
2024-11-09 17:53:43 +0700

    feat: add login page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,7 +18,7 @@ export function Header() {
         <nav className="flex items-center lg:pr-20 py-4">
          <ul className="nav-list justify-end hidden sm:flex space-x-4 ">
             <li className="hidden sm:flex space-x-4">
-            <a href="/login" className="px-9 py-2 rounded-full border border-slate-300 bg-white text-black hover:bg-gray-400 hover:text-white transition-colors">Log in</a>
+            <a href="#" onClick={() => navigate("/login")}  className="px-9 py-2 rounded-full border border-slate-300 bg-white text-black hover:bg-gray-400 hover:text-white transition-colors">Log in</a>
             </li>
             <li className="hidden md:flex space-x-4">
             <a href="/Signup" className="px-8 py-2  rounded-full border border-slate-300 bg-white text-black hover:bg-gray-400 hover:text-white transition-colors"> Sign up</a>
